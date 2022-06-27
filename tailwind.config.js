d70f69eeafcc90e066854dc655b32240de06a404
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-06-27 17:18:12 +0200

    fix wrong font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ 'public/**/*.html' ],
+  content: ['public/**/*.html'],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      "wireframe",
-      {
-        conshelv: {
-          "primary": "#84cc16",
-          "secondary": "#2F2d4b64",
-          "accent": "#d9f99d",
-          "neutral": "#e7e7e4",
-          "base-100": "#ffffff",
-          "info": "#3ABFF8",
-          "success": "#36D399",
-          "warning": "#FBBD23",
-          "error": "#F87272",
-        },
+    themes: [{
+      wireframe: {
+        ...require("daisyui/src/colors/themes")["[data-theme=wireframe]"],
+        "fontFamily": "Playfair Display", 
       },
+      conshelv: {
+        "primary": "#84cc16",
+        "secondary": "#2F2d4b64",
+        "accent": "#d9f99d",
+        "neutral": "#e7e7e4",
+        "base-100": "#ffffff",
+        "info": "#3ABFF8",
+        "success": "#36D399",
+        "warning": "#FBBD23",
+        "error": "#F87272",
+      },}
     ],
   },
 }
