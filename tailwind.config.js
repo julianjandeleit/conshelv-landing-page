83a4d1568ae4088a077705e8515ee870cf8ba138
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-12-24 00:04:37 +0100

    center icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,26 @@ module.exports = {
     extend: {
       fontFamily: {
         'manrope': ['"manrope"'],
+        'baskerville': ['"Libre Baskerville"'],
       },
+      colors: {
+        'other': '#9c8777'
+      }
     },
   },
   plugins: [require("daisyui")],
   daisyui: {
     themes: [{
       conshelv: {
-        "primary": "#EBE2D3",
-        "secondary": "#2F2d4b64",
-        "accent": "#d9f99d",
-        "neutral": "#e7e7e4",
+        "primary": "#5f5a52",
+        "secondary": "#9c8777",
+        "accent": "#2f2d4b64",
+        "neutral": "#f3ede2",
         "base-100": "#ffffff",
-        "info": "#52b5e0",
-        "success": "#4ebb93",
-        "warning": "#FBBD23",
-        "error": "#F87272",
+        "info": "#a36f5b",
+        "success": "#5aa53e",
+        "warning": "#fbc25b",
+        "error": "#ec255f",
         "fontFamily": "Anek Malayalam",
       },
     }
