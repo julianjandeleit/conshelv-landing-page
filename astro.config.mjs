c828fb211eb52d01a18a789287340a4449025e99
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2023-02-27 12:48:44 +0100

    redo base layout in astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+import yaml from '@rollup/plugin-yaml';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {plugins: [yaml()]}
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'manrope': ['"manrope"'],
+				'baskerville': ['"Libre Baskerville"'],
+			  },
+		},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [{
+		  conshelv: {
+			"primary": "#5f5a52",
+			"secondary": "#9c8777",
+			"accent": "#2f2d4b64",
+			"neutral": "#f3ede2",
+			"base-100": "#ffffff",
+			"info": "#a36f5b",
+			"success": "#5aa53e",
+			"warning": "#fbc25b",
+			"error": "#ec255f",
+			"fontFamily": "Anek Malayalam",
+		  },}]
+		}
+}
