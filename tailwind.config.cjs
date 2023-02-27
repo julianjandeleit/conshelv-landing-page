c06a7a83f063787fc0be2323e3c960c8502a8223
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2023-02-27 23:12:27 +0100

    small fixes and impressum

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,7 @@ module.exports = {
 		themes: [{
 		  conshelv: {
 			"primary": "#5f5a52",
-			"secondary": "#9c8777",
+			"secondary": "#22AED1",
 			"accent": "#2f2d4b64",
 			"neutral": "#f3ede2",
 			"base-100": "#ffffff",
