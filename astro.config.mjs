dcf21b274bab11b9f5adeba4ed03737b3379fe35
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2023-03-15 13:11:33 +0100

    add gtag using partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 import yaml from '@rollup/plugin-yaml';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(),    partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),],
   vite: {plugins: [yaml()]}
 });
